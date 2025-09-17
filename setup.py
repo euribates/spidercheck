a35f31b3079e30c67b7ebfa735406fe0d4edcab8
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2025-09-17 11:49:09 +0100

    Add files setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='Spidercheck',
+    description="Web server check spider",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='0.6',
+    license='MIT NON-AI',
+    author="Juan Ignacio Rodríguez de León",
+    author_email='euribates@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/euribates/spidercheck',
+    keywords='web spider django check quality',
+    install_requires=[
+          'scikit-learn',
+      ],
+
+)
